f4e1cbcfccdb35fca6812c6424b441479b5e878a
Jamie Phelps <james.phelps1995@live.com>
2020-06-06 12:19:06 +0100

    added queryselector for slideshow

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 const hamburger = document.querySelector('.toggle-btn');
 const mobileNavigation = document.querySelector('.toggled-nav-links');
 
+const slideShow = document.querySelector('.testimonials');
+
+slideButton.addEventListener('mouseover', (e) => {
+  slideShow.scrollLeft += 10;
+});
+
 hamburger.addEventListener('click', () => {
   if (mobileNavigation.style.display == 'block') {
     mobileNavigation.style.display = 'none';
