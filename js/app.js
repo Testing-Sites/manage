7d8fa3b6849f1fe1ce4979e5e048965db47d5759
Jamie Phelps <james.phelps1995@live.com>
2020-06-08 15:49:47 +0100

    added slideshow buttons functionality

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 const hamburger = document.querySelector('.toggle-btn');
 const mobileNavigation = document.querySelector('.toggled-nav-links');
 
-const slideShow = document.querySelector('.testimonials');
-const footerLogo = document.querySelector('#footerLogo');
+const slideShow = document.querySelector('.slide-show');
+let lwidth = window.screen.width;
 
-
+// mobile navigation
 hamburger.addEventListener('click', () => {
   if (mobileNavigation.style.display == 'block') {
     mobileNavigation.style.display = 'none';
@@ -12,3 +12,21 @@ hamburger.addEventListener('click', () => {
     mobileNavigation.style.display = 'block';
   }
 });
+
+// slideShow
+slideShow.addEventListener('click', (event) => {
+  if(event.target.id == "fButton") {
+    let fButton = event.target;
+    let slideShow = fButton.nextElementSibling;
+    if (fButton) {
+      slideShow.scrollLeft += lwidth;
+    }
+  }
+  if(event.target.id == "bButton") {
+    let bButton = event.target;
+    let slideShow = bButton.nextElementSibling.nextElementSibling;
+    if (bButton) {
+      slideShow.scrollLeft -= lwidth;
+    }
+  }
+})
